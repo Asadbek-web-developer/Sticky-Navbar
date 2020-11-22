a6337bda1fa05b0de2bd3f1ea9c237ae4d54573f
Asadbek <74813292+Asadbek-web-developer@users.noreply.github.com>
2020-11-22 23:31:25 +0500

    Sticky Navbar
    Using with Html CSS and JavaScript
    Sticky Navbar

---
+++ javascript.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+</script>
